35573d0768fb606d9449e150544125c643620548
Paweł Pieniacki <ppien@softserveinc.com>
2018-09-20 09:16:33 +0200

    added gitignore

--- dest/enums/operations.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var operations;
-(function (operations) {
-    operations["sum"] = "+";
-    operations["substraction"] = "-";
-    operations["multiplication"] = "*";
-    operations["division"] = "/";
-})(operations = exports.operations || (exports.operations = {}));
